cf691264905d2597fcc527d59f821c6d175ba613
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-28 01:04:27 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,16 @@ mod db;
 mod handler;
 mod model;
 mod route;
+mod config;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::error::Error;
+use std::io::Read;
+use std::net::SocketAddr;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
@@ -20,14 +19,26 @@ async fn manual_hello() -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+async fn main() -> Result<(), Box<dyn Error>> {
+    let base_port = 8000;
+    let max_retries = 10;
+
+    for port in base_port..(base_port + max_retries) {
+        let addr = SocketAddr::new("0.0.0.0".parse()?, port);
+        match HttpServer::new(|| {
+            App::new()
+                .service(hello)
+                .route("/hey", web::get().to(manual_hello))
+        })
+            .bind(addr) {
+            Ok(server) => {
+                println!("Started server on {}", addr);
+                server.run().await?;
+            }
+            Err(_) => {
+                continue;
+            }
+        }
+    }
+    Ok(())
 }
